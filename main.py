46d1d22016c8432a67a64e81fffb4a488156c53b
jakeeleslie <leslieje@udmercy.edu>
2022-01-30 10:56:13 -0800

    Project commit

---
+++ main.py
@@ -0,0 +1,30 @@
+#name = "Jake"
+
+# String interploration
+#print(f'Hello {name}')
+
+# Input function
+
+#print("Hello " + input("What is your name? "))
+
+#str = "Please answer my question"
+
+# Prints out the variable str's length
+#print(len(str))
+
+# Input function with len
+
+#print(len(input("Type out a sentence and I will tell you its length ")))
+
+# Project #1 band name generator
+
+#1. Create a greeting for your program
+print("Hello! This will be your tool to generate your band name\n")
+#2. Ask the user for the city that they grew up in.
+city = input("What city did you grow up in?\n ")
+#3. Ask the user for the name of a pet.
+pet = input("What is one of your pets name?\n")
+#4. Combine the name of their city and pet and shw them their band name.
+print("Your band name is: " + city + " " + pet)
+#5. Make sure the input cursor shows on a new line. I did this with \n
+
